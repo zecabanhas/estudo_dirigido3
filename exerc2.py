34bea9cc210163e5694e692e68df5a770bf4a90d
José Carlos <49734983+zecabanhas@users.noreply.github.com>
2019-06-26 19:57:40 -0300

    exercicio 2 lista 3

---
+++ exerc2.py
@@ -0,0 +1,5 @@
+#Faça um Programa que leia um vetor de 10 números reais e mostre-os na ordem inversa.
+vetor = [1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,1.10]
+vetor.reverse()
+for x in vetor:
+  print(x,end=" ")
